56864f4c103cc09f0b2b620001cc4f216ebee935
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-02-21 22:46:33 -0500

    Solutions of exercise 04

--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -1,38 +1,54 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
+function flatMapPromise(promise, asyncTransformer) {
   return new Promise((resolve, reject) => {
     promise
-      .then(/* IMPLEMENT ME! */);
+      .then(async value => {
+        await asyncTransformer(value).then(resp => {
+          resolve(resp);
+        }).catch(err => {
+          reject(err);
+        });
+        
+      })
+      .catch((err) => reject(err));
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
+  return firstPromise.then(async response => {
+    return await slowAsyncProcess(response);
+  });
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
  */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
-    /* IMPLEMENT ME! */
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+  return async function getUserByIdWithOrganization(userId) {
+    return await getUserById(userId).then(response => {
+      return getOrganizationById(response.organizationId).then(res => {
+        return {...response, organization: res};
+      });
+    }).catch(() => {
+      return undefined;
+    });
   };
 }
 
